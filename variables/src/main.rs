c2ecda7f5a75b3bfe2c797afed8223798d6301cd
Gooaine <gooaine@muon.page>
2026-01-07 11:58:31 +0900

    타입변경시 예제의 에러발생없음

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     // 변수
     let mut y = 5;
     println!("y : {y}");
-    mut y = 6;
+    y = 6;
     println!("y : {y}");
 
     let x = 5;
@@ -17,5 +17,9 @@ fn main() {
     // 상수
     const MYCONST: i32 = 100;
     println!("MYCONST : {MYCONST}");
+
+    // data type - 뭐지 사이트에서는 안된다고 헀는데 된다.
+    let _guess: u32 = "42".parse().expect("Not num");
     
+    // 스칼라타입: 단일값
 }
